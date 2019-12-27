f93be599a6b91e87e3dc3ecaba5c0c4dc7a95475
sanix-darker <s4nixd@gmail.com>
2019-12-27 15:19:23 +0100

    Official first commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+int main(int, char**)
+{
+    // Open default camera
+    cv::VideoCapture cap(0);
+
+    // Camera working?
+    if(!cap.isOpened()) {
+        std::cout << "Fix your camera!" << std::endl;
+        return -1;
+    }
+
+    cv::Mat frame;
+    cv::namedWindow("video", cv::WINDOW_AUTOSIZE);
+
+    while(1)
+    {
+        // Get a new frame from camera
+        cap >> frame;
+
+        // Show the frame
+        cv::imshow("video", frame);
+
+        // Exit
+        if(cv::waitKey(30) >= 0) break;
+    }
+
+    return 0;
+}
